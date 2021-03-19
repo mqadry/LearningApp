a855a496a56857c1d830c7cfc8d603c31931f7d4
mQadry <48778834+mqadry@users.noreply.github.com>
2021-03-19 22:48:50 +0200

    ok
    Final working code with google sign-in option

--- app/src/main/java/com/example/learningapp/MainActivity.java
+++ app/src/main/java/com/example/learningapp/MainActivity.java
@@ -1,17 +1,15 @@
 package com.example.learningapp;
 
 import android.content.Intent;
-//import android.support.annotation.NonNull;
-//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
@@ -23,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private GoogleApiClient googleApiClient;
     TextView textView;
     private static final int RC_SIGN_IN = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,14 +30,14 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         GoogleSignInOptions gso =  new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
-        googleApiClient=new GoogleApiClient.Builder(this)
+
+        googleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this,this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
                 .build();
 
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
 
-
-        signInButton=(SignInButton)findViewById(R.id.sign_in_button);
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,33 +45,36 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 startActivityForResult(intent,RC_SIGN_IN);
             }
         });
-
-
     }
-
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
+    {    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==RC_SIGN_IN){
+        if (requestCode == RC_SIGN_IN)
+        {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleSignInResult(result);
         }
     }
-    private void handleSignInResult(GoogleSignInResult result){
-        if(result.isSuccess()){
+
+    private void handleSignInResult(GoogleSignInResult result)
+    {
+        if (result.isSuccess())
+        {
             gotoProfile();
-        }else{
+        }
+        else {
             Toast.makeText(getApplicationContext(),"Sign in cancel",Toast.LENGTH_LONG).show();
         }
     }
-    private void gotoProfile(){
-        Intent intent=new Intent(MainActivity.this,ProfileActivity.class);
+
+    private void gotoProfile()
+    {
+        Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/learningapp/ProfileActivity.java
+++ app/src/main/java/com/example/learningapp/ProfileActivity.java
@@ -1,8 +1,6 @@
 package com.example.learningapp;
 
 import android.content.Intent;
-//import android.support.annotation.NonNull;
-//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,26 +26,26 @@ public class ProfileActivity extends AppCompatActivity implements GoogleApiClien
     ImageView profileImage;
     private GoogleApiClient googleApiClient;
     private GoogleSignInOptions gso;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        logoutBtn=(Button)findViewById(R.id.logoutBtn);
-        userName=(TextView)findViewById(R.id.name);
-        userEmail=(TextView)findViewById(R.id.email);
-        userId=(TextView)findViewById(R.id.userId);
-        profileImage=(ImageView)findViewById(R.id.profileImage);
+        logoutBtn = (Button) findViewById(R.id.logoutBtn);
+        userName = (TextView) findViewById(R.id.name);
+        userEmail = (TextView) findViewById(R.id.email);
+        userId = (TextView) findViewById(R.id.userId);
+        profileImage = (ImageView) findViewById(R.id.profileImage);
 
         gso =  new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
 
-        googleApiClient=new GoogleApiClient.Builder(this)
+        googleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this,this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
                 .build();
-
 
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,9 +54,12 @@ public class ProfileActivity extends AppCompatActivity implements GoogleApiClien
                         new ResultCallback<Status>() {
                             @Override
                             public void onResult(Status status) {
-                                if (status.isSuccess()){
+                                if (status.isSuccess())
+                                {
                                     gotoMainActivity();
-                                }else{
+                                }
+                                else
+                                    {
                                     Toast.makeText(getApplicationContext(),"Session not close",Toast.LENGTH_LONG).show();
                                 }
                             }
@@ -70,11 +71,15 @@ public class ProfileActivity extends AppCompatActivity implements GoogleApiClien
     @Override
     protected void onStart() {
         super.onStart();
-        OptionalPendingResult<GoogleSignInResult> opr= Auth.GoogleSignInApi.silentSignIn(googleApiClient);
-        if(opr.isDone()){
-            GoogleSignInResult result=opr.get();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(googleApiClient);
+        if (opr.isDone())
+        {
+            GoogleSignInResult result = opr.get();
             handleSignInResult(result);
-        }else{
+        }
+        else
+            {
             opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
                 @Override
                 public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
@@ -83,15 +88,18 @@ public class ProfileActivity extends AppCompatActivity implements GoogleApiClien
             });
         }
     }
+
     private void handleSignInResult(GoogleSignInResult result){
-        if(result.isSuccess()){
-            GoogleSignInAccount account=result.getSignInAccount();
+        if (result.isSuccess())
+        {
+            GoogleSignInAccount account = result.getSignInAccount();
             userName.setText(account.getDisplayName());
             userEmail.setText(account.getEmail());
             userId.setText(account.getId());
             try{
                 Glide.with(this).load(account.getPhotoUrl()).into(profileImage);
-            }catch (NullPointerException e){
+            }
+            catch (NullPointerException e){
                 Toast.makeText(getApplicationContext(),"image not found",Toast.LENGTH_LONG).show();
             }
 
@@ -99,10 +107,12 @@ public class ProfileActivity extends AppCompatActivity implements GoogleApiClien
             gotoMainActivity();
         }
     }
+
     private void gotoMainActivity(){
-        Intent intent=new Intent(this,MainActivity.class);
+        Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
     }
+
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
