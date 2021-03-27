0ee342ef50d87b2f0aac510a5e0a6851046fae72
mQadry <48778834+mqadry@users.noreply.github.com>
2021-03-27 23:12:38 +0200

    Working Sign-in
    Tested working sign in which display username, email, id and profile picture.

--- app/src/main/java/com/example/learningapp/MainActivity.java
+++ app/src/main/java/com/example/learningapp/MainActivity.java
@@ -1,69 +1,99 @@
 package com.example.learningapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+
+    LoginButton loginButton;
+    TextView userName, userID, userEmail;
+    ProfilePictureView profilePictureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
+
+        loginButton = findViewById(R.id.login_button);
+        userName = findViewById(R.id.userName);
+        userEmail = findViewById(R.id.userEmail);
+        userID = findViewById(R.id.userID);
+        profilePictureView = findViewById(R.id.friendProfilePicture);
+
+        loginButton.setReadPermissions(Arrays.asList("email"));
+        callbackManager = CallbackManager.Factory.create();
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                AccessToken currentAccessToken = AccessToken.getCurrentAccessToken();
+                if (currentAccessToken != null)
+                {
+                    GraphRequest request = GraphRequest.newMeRequest(
+                            currentAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+                                @Override
+                                public void onCompleted(JSONObject object, GraphResponse response) {
+                                    try {
+                                        String first_name = object.getString("first_name");
+                                        String last_name = object.getString("last_name");
+                                        String email = object.getString("email");
+                                        String id = object.getString("id");
+
+                                        userName.setText("Name: " + first_name + " " + last_name);
+                                        userEmail.setText("Email: " + email);
+                                        userID.setText("ID: " + id);
+                                        profilePictureView.setProfileId(id);
+
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                }
+                            });
+
+                    Bundle parameters = new Bundle();
+                    parameters.putString("fields", "first_name,last_name,email,id");
+                    request.setParameters(parameters);
+                    request.executeAsync();
+                }
+
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
-//        GoogleSignInOptions gso =  new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//                .requestEmail()
-//                .build();
-//
-//        googleApiClient = new GoogleApiClient.Builder(this)
-//                .enableAutoManage(this,this)
-//                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
-//                .build();
-//
-//        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
-//
-//        signInButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-//                startActivityForResult(intent,RC_SIGN_IN);
-//            }
-//        });
-//    }
-
-//    @Override
-//    public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
-//    {    }
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode == RC_SIGN_IN)
-//        {
-//            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-//            handleSignInResult(result);
-//        }
-//    }
-//
-//    private void handleSignInResult(GoogleSignInResult result)
-//    {
-//        if (result.isSuccess())
-//        {
-//            gotoProfile();
-//        }
-//        else {
-//            Toast.makeText(getApplicationContext(),"Sign in cancel",Toast.LENGTH_LONG).show();
-//        }
-//    }
-//
-//    private void gotoProfile()
-//    {
-//        Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
-//        startActivity(intent);
-//    }
-//}
